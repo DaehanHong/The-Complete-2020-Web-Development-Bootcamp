2855e73492fca46f467bd2e5687b987972bd9979
Daehan Hong <200307118@student.georgianc.on.ca>
2020-03-01 19:34:32 -0500

    Final Uploading

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res) {
+app.use(bodyParser.urlencoded({extended: true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e72ca729af228beabd5d20e3b7749713&units=metric";
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  const query = req.body.cityName;
+  const apiKey = "e72ca729af228beabd5d20e3b7749713";
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
   https.get(url, function(response) {
     console.log(response.statusCode);
@@ -13,9 +22,13 @@ app.get("/", function(req, res) {
     response.on("data", function(data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
-      console.log(temp);
       const description = weatherData.weather[0].description;
-      console.log(description);
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<p>The weather is currently " + description + "</p>");
+      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
+      res.write("<img src=" + imageURL + ">");
+      res.send();
       // const object = {
       //   name: "Daehan",
       //   favouriteFood: "Ramen"
@@ -24,10 +37,7 @@ app.get("/", function(req, res) {
 
     });
   });
-
-  res.send("Server is up and running.");
 })
-
 
 
 
